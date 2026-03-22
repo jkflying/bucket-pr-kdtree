96c2fda6797e5c7ccfc4ccbb9522ee707266ba18
Julian Kent <jkflying@gmail.com>
2026-03-22 14:18:27 +0100

    clang-format already installed

--- C++/tools/ci.sh
+++ C++/tools/ci.sh
@@ -3,8 +3,8 @@ set -euo pipefail
 
 CPP_DIR="$(cd "$(dirname "$0")/.." && pwd)"
 
-if ! command -v clang-format &>/dev/null; then
-    sudo apt-get install -y clang-format libnanoflann-dev
+if ! dpkg -s libnanoflann-dev &>/dev/null; then
+    sudo apt-get install -y libnanoflann-dev
 fi
 
 # clang-format check
